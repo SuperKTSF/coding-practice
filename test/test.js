8a6fdd1b2cd5a396231b2319769f1ed802e75386
SuperKTSF <15051966835@163.com>
2017-03-15 14:16:40 +0800

    2017.03.15

--- test/test.js
+++ test/test.js
@@ -1,38 +1,7 @@
-function NumberOf1Between1AndN_Solution(n,m)
-{
-    var temp=1;
-    var nums=0;
-    while(Math.floor(n/temp)){
-		if(m!=0){
-			var  ab = Math.floor(n/(temp*10));
-			nums+=ab*temp;  //第一部分的次数
-			var c = Math.floor(n%(temp*10)/temp);
-			if(c>m){    //第二部分的次数
-				nums+=temp;
-			}else if(c==m){
-            nums+=(n%temp+1);
-			}
-			temp*=10;			
-		}else{
-			var  ab = Math.floor(n/(temp*10));
-			nums+=(ab-1)*temp;  //第一部分的次数
-			var c = Math.floor(n%(temp*10)/temp);
-			if(c>m){    //第二部分的次数
-				nums+=temp;
-			}else if(c==m){
-            nums+=(n%temp+1);
-			}
-			temp*=10;
-		}
-    }
-    return nums;   
-}
-
-function a(n){
-		var str="";
-	for(var i =0;i<=9;i++){
-		str += NumberOf1Between1AndN_Solution(n,i)+" ";
-	}
-	console.log(str.trim());
+function containsNumber(str) {
+    var arr = str.split("");
+	arr.map(function(item){
+        return (typeof parseInt(item) =="number");
+    });
 }
 
